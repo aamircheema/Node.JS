f82569f414b003c6db584e1472b81a814ac1303d
Aamir Cheema <amircheema295@gmail.com>
2023-10-16 15:19:20 +0500

    path methods

---
+++ reference/path_demo.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+// Base file name
+console.log(path.basename(__filename));
+// path_demo.js
+
+//Directory  name
+console.log(path.dirname(__filename));
+//C:\Users\HP\Desktop\node.js\reference
+
+//File extension
+console.log(path.extname(__filename));
+// .js
+
+// Creat path object
+console.log(path.parse(__filename).base);
+//path_demo.js
+
+//Concatinate paths
+console.log(path.join(__dirname, "test", "hello.html"));
